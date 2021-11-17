16c5a381b8dd874dfbd71db41bd5d2618394a679
Roshan Acharya <acharyaroshan2357@gmail.com>
2021-11-17 21:58:41 +0545

    added .well-known folder

--- ulka-config.js
+++ ulka-config.js
@@ -1,23 +1,23 @@
 module.exports = {
   siteMetaData: {
-    title: 'Ulka',
-    description: 'Documentation site for ulkajs',
-    domain: 'https://ulka.js.org',
+    title: "Ulka",
+    description: "Documentation site for ulkajs",
+    domain: "https://ulka.js.org",
   },
-  buildPath: 'build',
-  pagesPath: 'pages',
-  templatesPath: 'templates',
+  buildPath: "build",
+  pagesPath: "pages",
+  templatesPath: "templates",
   plugins: [
-    'ulka-source-ejs',
-    'ulka-plugin-sitemap',
+    "ulka-source-ejs",
+    "ulka-plugin-sitemap",
     {
-      resolve: 'ulka-plugin-autolink-headers',
+      resolve: "ulka-plugin-autolink-headers",
       options: {
         hLevels: [1, 2, 3],
       },
     },
     {
-      resolve: 'ulka-plugin-minifier',
+      resolve: "ulka-plugin-minifier",
       options: {
         html: {
           conservativeCollapse: true,
@@ -27,16 +27,16 @@ module.exports = {
   ],
   contents: [
     {
-      path: 'docs',
-      generatePath: 'docs',
-      template: 'docs.ulka',
-      name: 'docs',
+      path: "docs",
+      generatePath: "docs",
+      template: "docs.ulka",
+      name: "docs",
     },
     {
-      path: 'tutorial',
-      generatePath: 'tutorial',
-      template: 'docs.ulka',
-      name: 'tutorial',
+      path: "tutorial",
+      generatePath: "tutorial",
+      template: "docs.ulka",
+      name: "tutorial",
     },
   ],
-}
+};
